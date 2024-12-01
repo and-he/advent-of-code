72b7c2375854597f91ad6789bed2357415a861e7
Andy <heandy11@yahoo.com>
2024-11-30 21:20:08 -0800

    initial commit

---
+++ 2023/day1/go/dec-1-23.go
@@ -0,0 +1,60 @@
+// ownerproof-4275689-1732682827-7fe4bff08805
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func get_calibration(line string) (first int, last int) {
+	left, right := 0, len(line)-1
+	for left < len(line) {
+		if '0' <= line[left] && line[left] <= '9' {
+			first_conv, err := strconv.Atoi(string(line[left]))
+			if err != nil {
+				log.Fatalf("error converting %s at index %d to int", line, left)
+			}
+			first = first_conv
+			break
+		} else {
+			left += 1
+		}
+	}
+	for right >= 0 {
+		if '0' <= line[right] && line[right] <= '9' {
+			last_conv, err := strconv.Atoi(string(line[right]))
+			if err != nil {
+				log.Fatalf("error converting %s at index %d to int", line, right)
+			}
+			last = last_conv
+			break
+		} else {
+			right -= 1
+		}
+	}
+	return
+}
+
+func main() {
+	file, err := os.Open("day1_input.txt")
+	if err != nil {
+		log.Fatalf("error reading input file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		first, last := get_calibration(scanner.Text())
+		calibration := (first * 10) + last
+		sum += calibration
+		fmt.Printf("%s ---> len=%d ---> first=%d, last=%d, calibration=%d\n", scanner.Text(), len(scanner.Text()), first, last, calibration)
+	}
+	fmt.Printf("sum is %d", sum)
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error in scanner: %v", err)
+	}
+}
